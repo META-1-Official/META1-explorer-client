afefd43c329c18cbcd5980555b4af1f5cf530c13
buddy1014 <developer961014@gmail.com>
2022-01-20 22:19:52 -0500

    create a Tab component

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,29 @@
-import WidgetsIcon from '@mui/icons-material/Widgets';
+import {useState} from 'react';
 
-import {
-  FilledLineChartCard,
-  LineChartCard,
-  SearchCard,
-} from '../../components/Card';
+import {Tab} from '../../components/Tab';
+
+const tabs = [
+  {
+    value: 'operations',
+    label: 'Operations',
+  },
+  {
+    value: 'markets',
+    label: 'Markets',
+  },
+  {
+    value: 'holders',
+    label: 'Holders',
+  },
+];
 
 export default function Dashboard() {
+  const [activeTab, setActiveTab] = useState('operations');
+
   return (
     <div className="page" style={{padding: '2em'}}>
-      <LineChartCard
+      <Tab tabs={tabs} activeTab={activeTab} onChangeTab={setActiveTab} />
+      {/* <LineChartCard
         title="Block Number"
         number={8275717}
         icon={<WidgetsIcon sx={{color: '#ffc000'}} />}
@@ -21,7 +35,7 @@ export default function Dashboard() {
         searchInputLabel="Block number"
         searchInputPlaceholder="Enter block number"
       />
-      <FilledLineChartCard />
+      <FilledLineChartCard /> */}
     </div>
   );
 }
